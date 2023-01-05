423607c4e7c81a30715850f23a0d7917e930013a
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-01-05 16:34:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2) return false;
+  let brackets = {}
+  bracketsConfig.forEach(i => {brackets[i[0]] = i[1]});
+  let stack = [];
+  for(let i = 0; i < str.length; i++)
+    if(brackets[str[i]] && (brackets[str[i]] != str[i] || !stack.includes(str[i])))
+      stack.push(str[i])
+    else if(brackets[stack[stack.length-1]] == str[i])
+      stack.pop();
+    else
+      return false;
+  
+  return true;
 }
